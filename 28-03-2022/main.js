c751a9cbadd8e4a4e2a6d17d04bd17a2c43e474c
yousefalhindawi <eng.yousefalhindawi@gmail.com>
2022-03-30 22:17:47 +0300

    main

--- 28-03-2022/main.js
+++ 28-03-2022/main.js
@@ -5,17 +5,17 @@ let tbody = document.querySelector("#tbody")
 let show =document.querySelector('#show')
 let hide =document.querySelector('#hide')
 show.addEventListener('click',function() {
-    let dataNew = JSON.parse(data.responseText);
+    let dataNew = JSON.parse(data.responseText); // convert string to array of object
     // console.log(dataNew);
-    let Data ='';
+    let tbodyData ='';
     for (let i = 0; i < dataNew.length; i++){
-        Data +=`<tr><td>${i}</td>
+        tbodyData +=`<tr><td>${i}</td>
         <td>${dataNew[i].brand}</td>
         <td>${dataNew[i].model}</td>
         <td><img src="${dataNew[i].URL}"></td>
         </tr>`
     }
-    tbody.innerHTML = Data; 
+    tbody.innerHTML = tbodyData; 
 })
 
 hide.addEventListener('click',function() {
